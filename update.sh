6b5e9fa5a642708c3254fe6ee0e722446cdb871b
BOYPT <pentie@gmail.com>
2013-11-14 15:21:56 +0800

    normal backup

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 current_path=`dirname $(readlink -f $0)`
-current_dev=/srv/http/public_html/forms/wp-content/themes/twentythirteen
 
+#从开发目录复制到本目录
+current_dev=/srv/http/public_html/forms/wp-content/themes/twentythirteen
 cp $current_dev/page-customize-list.php $current_path
 cp $current_dev/page-user-counter.php $current_path
 cp $current_dev/client-user-counter.js $current_path
 
+#更新到github备份
 git add .
 git commit -m 'normal backup'
 git push
